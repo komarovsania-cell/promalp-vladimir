84e9df7e6e613f5dd5aa3265ba03590c91516cd0
VZL <komarov.sania@gmail.com>
2026-08-02 18:11:54 +0300

    Add site configuration with company details

---
+++ src/lib/site.ts
@@ -0,0 +1,11 @@
+export const site = {
+  companyName: "ВинСтарКом",
+  phoneDisplay: "+7 (915) 768-24-99",
+  phoneHref: "tel:+79157682499",
+  whatsappHref: "https://wa.me/79157682499",
+  telegramHref: "https://t.me/+79157682499",
+  region: "Владимирская область",
+  city: "Владимир",
+  workHours: "Пн–Сб, 08:00–18:00",
+  minOrder: "5 000 ₽",
+};
