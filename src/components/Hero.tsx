b3b0bb026415c358262cb9d58928b1eba7a3d5b3
VZL <komarov.sania@gmail.com>
2026-08-04 21:21:22 +0300

    Add files via upload

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -91,7 +91,7 @@ export default function Hero() {
           </div>
         </div>
 
-        <div className="relative hidden lg:flex items-center justify-center" style={{ transform: "translateX(1cm)" }}>
+        <div className="relative hidden lg:flex items-center justify-center" style={{ transform: "translateX(2.5cm)" }}>
           <div className="hero-photo-card relative w-full aspect-[3/4] max-w-md rounded-[28px] border border-ink-border overflow-hidden">
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img
