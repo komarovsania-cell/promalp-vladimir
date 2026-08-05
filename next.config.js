80e13e5e6a8e7aca0aaf8831cf35362c4eaee08b
VZL <komarov.sania@gmail.com>
2026-08-05 10:09:30 +0300

    Add standalone output configuration to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    eslint: {
-          ignoreDuringBuilds: true,
-    },
+      reactStrictMode: true,
+      eslint: {
+              ignoreDuringBuilds: true,
+      },
+      output: "standalone",
 };
 
 module.exports = nextConfig;
