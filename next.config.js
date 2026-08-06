b5a8eb06957eb94e4c8f2ef83960da94e6db6b79
VZL <komarov.sania@gmail.com>
2026-08-06 19:27:58 +0300

    Fix formatting in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,9 +2,8 @@
 const nextConfig = {
       reactStrictMode: true,
       eslint: {
-              ignoreDuringBuilds: true,
+            ignoreDuringBuilds: true,
       },
-      output: "standalone",
 };
 
 module.exports = nextConfig;
